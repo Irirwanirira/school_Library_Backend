6230c7abd5e5b376de60a193e6828b8c019e1a40
Irirwanirira <irijoseph41@gmail.com>
2023-10-07 11:01:40 +0200

    add necessary dependence

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,67 +1,68 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
-const{findUserByEmail, createUser}= require("../../service/auth.service");
-const { hashPassword, generateToken } = require("../../utils/auth");
+const { findUserByEmail, createUser } = require('../../service/auth.service');
+const { hashPassword, generateToken } = require('../../utils/auth');
 
 export const loginUser = async (req, res) => {
-    const {email, password} = req.body;
-    const user = await findUserByEmail(email);
+  const { email, password } = req.body;
+  const user = await findUserByEmail(email);
 
-    try {
-        if(!user){
-            return res.status(404).send({message: "User not found. Please register"});
-        };
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if(!isPasswordValid){
-            return res.status(400).send({message: "Invalid password"});
-        };
-
-        const generatedToken = generateToken(user);
-        return res.status(200).send({
-            message: "User logged in successfully",
-            token: generatedToken,
-            user: user
-        });
-
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-        
+  try {
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: 'User not found. Please register' });
     }
-}
 
-export const registerUser = async (req,res) => {
-    const {name, email, password} = req.body;
-    try {
-        const oldUser = findUserByEmail(email);
-        if(oldUser) {
-            return res.status(409).send({message: "User already exist. Please login"});
-        };
-
-        const hashedPassword = await hashPassword(password);
-        const newUser = await createUser({email: email.toLowerCase(), password: hashedPassword, name });
-        if(!newUser){
-            return res.status(500).send({message:"Error creating user"});
-        };
-
-        const response = {
-            id: newUser.id,
-            name: newUser.name,
-            email: newUser.email,
-            createdAt: newUser.createdAt,
-            updatedAt: newUser.updatedAt
-        }
-
-        const generatedToken = generateToken(newUser);
-        return res.status(201).send({
-            message: "User created successfully",
-            token: generatedToken,
-            user: response
-        });
-
-    } catch (error) {
-        return res.status(500).send({message: error.message});
-        
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).send({ message: 'Invalid password' });
     }
-}
 
+    const generatedToken = generateToken(user);
+    return res.status(200).send({
+      message: 'User logged in successfully',
+      token: generatedToken,
+      user: user,
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const oldUser = await findUserByEmail(email);
+    if (oldUser) {
+      return res
+        .status(409)
+        .send({ message: 'User already exist. Please login' });
+    }
+
+    const hashedPassword = await hashPassword(password);
+    const newUser = await createUser({
+      email: email.toLowerCase(),
+      password: hashedPassword,
+      name,
+    });
+    if (!newUser) {
+      return res.status(500).send({ message: 'Error creating user' });
+    }
+    const response = {
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+      updatedAt: newUser.updatedAt,
+    };
+    const generatedToken = generateToken(newUser);
+    return res.status(201).send({
+      message: 'User created successfully',
+      token: generatedToken,
+      user: response,
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
