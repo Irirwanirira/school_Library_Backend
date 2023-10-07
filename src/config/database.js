3e06cf2975f0ee1b5ae144f3287b44892a695f9d
Irirwanirira <irijoseph41@gmail.com>
2023-10-07 10:13:30 +0200

    configure database

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv').config() ;
+
+module.exports = {  
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    dialect: process.env.DIALECT,
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+};
