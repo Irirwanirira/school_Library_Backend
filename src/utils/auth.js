be6da1a1081c5d20afc06197b6a2af7184b657c0
Irirwanirira <irijoseph41@gmail.com>
2023-10-07 10:15:08 +0200

    add bcrypt and jwt important function

---
+++ src/utils/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+export const generateToken = (user) => {
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        }
+    );
+    return token;
+};
+
+export const hashPassword = async(password) =>{
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword =  bcrypt.hash(password, salt);
+    return hashedPassword;
+};
+
