2a6cf0b203264c35fba5f567486a5eeb920456b1
Irirwanirira <irijoseph41@gmail.com>
2023-10-07 10:13:02 +0200

    Add auth routes

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
-// const routes = require('./routes');
+const authRoutes = require('./src/routes/auth');
 
 const app = express();
 const { NODE_ENV, port = 7000 } = process.env;
 
 const isProduction = NODE_ENV === 'production';
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(helmet());
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-// app.use(routes);
+// app.use((err, req, res) => {
+//   res.status(500).json({
+//     message: 'Internal server error',
+//     error: isProduction ? null : err,
+//   });
+// });
 
-app.use((err, req, res) => {
-  res.status(500).json({
-    message: 'Internal server error',
-    error: isProduction ? null : err,
-  });
-});
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
