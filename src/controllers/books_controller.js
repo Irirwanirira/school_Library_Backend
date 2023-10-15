308d5fd45bb1f06fc444e48bf1d4111c2e549ab9
Irirwanirira <irijoseph41@gmail.com>
2023-10-15 08:48:40 +0200

    add books controller

--- src/controllers/books_controller.js
+++ src/controllers/books_controller.js
@@ -1,10 +1,16 @@
+const Book = require('../database/models/index')
+const User = require('../database/models/index')
+const Reservation = require('../database/models/index')
+
+
 const {createBook, findAllBooks, findBookById,  deleteBook} = require('../service/books');
 
-export const createBookController = async (req, res) => {
-    const book = req.body;
-    const newBook = await createBook(book);
-    return res.status(201).send({message:"Books created successfully"});
-};
+// export const createBookController = async (req, res) => {
+//     const userId = req.params.userId;
+//     const book = req.body;
+//     const newBook = await createBook({...book, userId});
+//     return res.status(201).send({message:"Books created successfully"});
+// };
 
 export const getAllBooks = async(req, res) => {
     const allBooks = await findAllBooks();
@@ -28,3 +34,17 @@ export const removeBook = async (req, res) => {
         message: `${removed} was successfully deleted`,
     })
 };
+
+export const booksWithUsers = async() => {
+    await Book.findAll({
+        include: {
+            model: User,
+             through: {Reservation,
+            where: {
+                User_id:[1,2,3,4]
+                }
+            }
+        }
+    })
+}
+console.log(booksWithUsers());
